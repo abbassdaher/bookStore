1130ba25ce7134f9aaf1f69df0ff79e2ac06e829
unknown <abbass.daher1@outlook.com>
2025-02-11 01:07:00 +0200

    install mongoose and create connect to mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const booksPath = require("./routes/books");
-const authorsPath = require("./routes/authors")
+const authorsPath = require("./routes/authors");
+const mongoose = require("mongoose");
+
+// connection with mongoDB
+mongoose.connect("mongodb://localhost/bookStoreDB").then(() => {
+  console.log("connected to MongoDB");
+}).catch(() => {console.log("connection faild to mongoDB");
+
+  
+}
+)
 
 // app init
 const app = express();
@@ -10,7 +20,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/books", booksPath);
-app.use("/api/author",authorsPath)
+app.use("/api/author", authorsPath);
 
 // Start the server
 const PORT = 5000;
